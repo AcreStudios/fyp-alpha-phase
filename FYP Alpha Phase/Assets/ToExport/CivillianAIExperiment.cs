9e9d62cc48816f979951558ad78ea8acab17af1f
zlurker <tallspin_97@hotmail.com>
2016-09-10 12:55:07 +0800

    Anim SetUp ect
    Setting up animations for AI's

--- FYP Alpha Phase/Assets/ToExport/CivillianAIExperiment.cs
+++ FYP Alpha Phase/Assets/ToExport/CivillianAIExperiment.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CivillianAIExperiment : MonoBehaviour {
 
-    public enum CivillianStates
-    {
+    public enum CivillianStates {
         Calm, Panic, Alert
     }
 
@@ -16,74 +16,75 @@ public class CivillianAIExperiment : MonoBehaviour {
     Vector3 destination;
     RaycastHit hit;
 
-    void Start () {
-        //currentState = CivillianStates.Calm;
+    Collider placeToHide;
+    Collider lastObs;
 
+    Animator animator;
+
+    void Start() {
+        //currentState = CivillianStates.Calm;
+        destination = transform.position;
         agent = GetComponent<NavMeshAgent>();
-	}
-	
-	void Update () { 
-        switch (currentState)
-        {
+
+        animator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+    }
+
+    void Update() {
+        switch (currentState) {
             case CivillianStates.Calm:
-                Debug.Log("Do nothing, talk to other AI");
+                animator.SetInteger("TreeState", Random.Range(0, 2));
                 break;
 
             case CivillianStates.Panic:
-                if ((destination - transform.position).magnitude < 5){
-                    if (Physics.Linecast(transform.position,target.position, out hit))
-                    {
-                        if (hit.transform == target)
-                        {
+                if ((destination - transform.position).magnitude < 1) {
+                    animator.SetInteger("TreeState", 2);
+                    if (Physics.Linecast(transform.position, target.position, out hit)) {
+                        if (hit.transform == target) {
                             destination = HuntForHidingSpot();
+                            //Debug.Log("Working");
+                            
                         }
                     }
-                    Debug.Log("Doing nothing, drawing linecast");
-                }
-                else
-                {
+                } else {
                     agent.destination = destination;
+                    animator.SetInteger("TreeState", 3);
                 }
-                
+
                 break;
 
             case CivillianStates.Alert:
-                if ((target.position - transform.position).magnitude < detectionRadius)
-                {
+                if ((target.position - transform.position).magnitude < detectionRadius) {
                     destination = HuntForHidingSpot();
                     currentState = CivillianStates.Panic;
                 }
-
+                animator.SetInteger("TreeState", 0);
                 Debug.Log("Player not in range, but AI on alert"); //Maybe play a heightened talking anim
                 break;
         }
-	}
+    }
 
-    Vector3 HuntForHidingSpot()
-    {
-        Collider placeToHide = null;
+    Vector3 HuntForHidingSpot() {
+
         Collider[] temp;
 
         float dist = Mathf.Infinity;
 
         temp = Physics.OverlapSphere(transform.position, detectionRadius);
 
-        if (temp.Length > 0)
-        {
+        if (temp.Length > 0) {
 
-            foreach (Collider obs in temp)
-            {
-                if (obs.transform.tag == "Obstacles")
-                {
-                    float tempDist = (obs.transform.position - transform.position).magnitude;
-                    if (tempDist < dist)
-                    {
-                        dist = tempDist;
-                        placeToHide = obs;
+            foreach (Collider obs in temp) {
+                if (obs.transform.tag == "Obstacles") {
+                    if (obs != lastObs) {
+                        float tempDist = (obs.transform.position - transform.position).magnitude;
+                        if (tempDist < dist) {
+                            dist = tempDist;
+                            placeToHide = obs;
+                        }
                     }
                 }
             }
-
+            lastObs = placeToHide;
             return placeToHide.ClosestPointOnBounds(target.position) + ((placeToHide.bounds.center - placeToHide.ClosestPointOnBounds(target.position)) * 2);
         }
         return transform.position; //Maybe return a further location for AI to run away 
